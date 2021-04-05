a0b7174d0734c5ce2539dda427ac5818eb733f15
Brandon <bmark89@uw.edu>
2021-04-05 15:51:30 -0700

    removed batch_roots for reorg

--- synapses/connectivity_utils.py
+++ synapses/connectivity_utils.py
@@ -1,10 +1,10 @@
 import sys
 if '/Users/brandon/Documents/Repositories/Python/FANC_auto_recon/transforms/' not in sys.path:
     sys.path.append('/Users/brandon/Documents/Repositories/Python/FANC_auto_recon/transforms/')
-from fanc_seg_utils.annotation_interface import neuroglancer_utilities,schema_download,schema_upload,statemanager
-from fanc_seg_utils.skeletons import catmaid_utilities,skeleton_manipulations,skeletonization
+from .. import neuroglancer_utilities
+from ..annotations import schema_download,schema_upload,statemanager
+from ..skeletonization import catmaid_utilities,skeleton_manipulations,skeletonization
 
-import connectivity_utils
 
 import pandas as pd
 import numpy as np
@@ -58,22 +58,36 @@ def get_partner_synapses(root_id,df,direction='inputs',threshold=None):
     return(partners)
 
 
+
+
+
+def batch_partners(fname,root_id,direction,threshold=None):
+
+    result = pd.DataFrame(columns=['post_id', 'pre_pt', 'post_pt', 'source', 'pre_id'])
+
+    for chunk in pd.read_csv(fname, chunksize=10000):
+        chunk_result = get_partner_synapses(root_id,chunk,direction=direction,threshold=threshold)
+        if len(chunk_result) > 0:
+            result = result.append(chunk_result, ignore_index=True)
     
-def batch_roots(cv,df,n=100000):
-    groups = int(np.ceil(len(df)/n))
-    full = []
-    df = df.join(pd.DataFrame(np.ones([len(df),2]),columns={'pre_roots','post_roots'},dtype='int64'))
-    for i in range(groups+1):
-        start = n*i
-        stop = n*(i+1)
-        pre = cv.get_roots(df.pre_id.values[start:stop])
-        post = cv.get_roots(df.post_id.values[start:stop])
-        try:
-            df.loc[start:stop-1,'pre_roots'] = pre
-            df.loc[start:stop-1,'post_roots'] = post
-        except:
-            print('Failed at:',start)
-            return(df)
-        print(start,stop)
-    return(df)
+    return(result)
+
+def get_partner_synapses(root_id,df,direction='inputs',threshold=None):
+    if direction == 'inputs':
+        to_find = 'post_id'
+        to_threshold = 'pre_id'
         
+    elif direction == 'outputs':
+        to_find = 'pre_id'   
+        to_threshold = 'post_id'
+    
+    partners = df.loc[df[to_find]==root_id]
+        
+    if threshold is not None:
+        counts = partners[to_threshold].value_counts()
+        t_idx = counts >= threshold
+        
+        partners = partners[partners[to_threshold].isin(set(t_idx.index[t_idx==1]))]
+
+    
+    return(partners)
