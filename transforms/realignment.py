4d81a8e672447125221f52a44a6d94934a9f7e18
Jasper Phelps <jtmaniatesselvin@g.harvard.edu>
2021-04-23 16:51:02 -0400

    fanc4_to_3 now outputs nx3 arrays instead of 3xn arrays. Added some tests and docs.

--- transforms/realignment.py
+++ transforms/realignment.py
@@ -11,48 +11,63 @@ def fanc4_to_3(points, scale=2, return_dict=False):
     '''
     Convert from realigned dataset to original coordinate space.
     Args:
-             points: an nx3 array of mip0 voxel coordinates
-             scale:  selects the granularity of the field being used, but does not change the units.
-             return_dict: If false, returns an nx3 numpy array. If false,
-                          returns a dict containing x, y, z, dx, and dy values.
+        points:      an nx3 array of mip0 voxel coordinates
+        scale:       selects the granularity of the field being used, but
+                         does not change the units.
+        return_dict: If false, returns an nx3 numpy array. If false,
+                         returns a dict containing x, y, z, dx, and dy values.
     
-    Returns: a dictionary of transformed x/y/z values and the dx/dy/dz values
+    Returns:
+        (default) an nx3 numpy array containing the transformed points
+        (return_dict=True) a dictionary of transformed x/y/z values and the
+            dx/dy values.
+        Returned coordinates are floats (i.e. can have fractional values)
     '''
              
     base = 'https://spine.janelia.org/app/transform-service/dataset/fanc_v4_to_v3/s/{}/'.format(scale)
+
+    n = len(np.shape(points))
                       
-    if len(np.shape(points)) > 1:
-        # JSP: This code block doesn't work for me
+    if n == 1:
+        full_url = (base + 'z/{}/'.format(str(int(points[2])))
+                         + 'x/{}/'.format(str(int(points[0])))
+                         + 'y/{}/'.format(str(int(points[1]))))
+        r = requests.get(full_url)
+    else:
         full_url = base + 'values_array'
         points_dict = {'x': list(points[:,0].astype(str)),
                        'y': list(points[:,1].astype(str)),
                        'z': list(points[:,2].astype(str))}
         r = requests.post(full_url, json=points_dict)
-    else:
-        full_url = (base + 'z/{}/'.format(str(int(points[2])))
-                         + 'x/{}/'.format(str(int(points[0])))
-                         + 'y/{}/'.format(str(int(points[1]))))
-        r = requests.get(full_url)
-    
+
     try:
         r = r.json()
         if return_dict:
             return r
         else:
-            return np.array((r['x'], r['y'], r['z']))
+            if n == 1:
+                return np.array((r['x'], r['y'], r['z']))
+            else:
+                return np.array((r['x'], r['y'], r['z'])).T
     except:
         return r
 
 
 def fanc3_to_4(pts, mode='descent', precision=1.5, verbose=False):
     """
-    pts:       nx3 numpy array of xyz-ordered points
-    mode:      'descent' or 'inversefield'
-               Use 'descent' if the v3 to v4 alignment field isn't available.
-               Use 'inversefield' if it is and you want faster (but potentially
-                   less accurate?) mappings.
-    precision: when in 'descent' mode, how accurate of an inverse must be
-               found before descent stops. In units of mip0 pixels.
+    Args:
+        pts:       nx3 numpy array of xyz-ordered points
+        mode:      'descent' or 'inversefield'
+                   Use 'descent' if the v3 to v4 alignment field isn't available.
+                   Use 'inversefield' if it is and you want faster (but
+                       potentially less accurate?) mappings.
+        precision: when in 'descent' mode, how accurate of an inverse must be
+                   found before descent stops. In units of mip0 pixels.
+
+    Returns:
+        An nx3 numpy array containing the transformed points. Returned
+        coordinates are ints. (Subpixel interpolation could be implemented for
+        slightly more precision but hasn't been done yet.)
     """
     if isinstance(pts, (list, tuple)):
         pts = np.array(pts)
@@ -100,24 +115,22 @@ def fanc3_to_4(pts, mode='descent', precision=1.5, verbose=False):
         raise ValueError("mode must be 'descent' or 'inversefield' but was {}".format(mode))
 
 
+# Some example points to test
+test_pts_v3 = np.array([
+    [5207, 98758, 3028],
+    [23540, 116496, 1228]
+], dtype=np.uint32)
+test_pts_v4 = np.array([
+    [5286, 98834, 3028],
+    [23622, 116520, 1228]
+], dtype=np.uint32)
+
 def test_fanc3_to_4(verbose=True):
-    # Some example point to test
-    pts_v3 = np.array([
-        [5207, 98758, 3028],
-        [23540, 116496, 1228]
-    ], dtype=np.uint32)
-    # v4 locations that actually correspond to the ones above (ground truth)
-    pts_v4 = np.array([
-        [5286, 98834, 3028],
-        [23622, 116520, 1228]
-    ], dtype=np.uint32)
-
-
-    pts3to4 = fanc3_to_4(pts_v3, verbose=verbose)
-    error = pts3to4 - pts_v4
+    pts3to4 = fanc3_to_4(test_pts_v3, verbose=verbose)
+    error = pts3to4 - test_pts_v4
 
     print('True v4 points:')
-    print(pts_v4)
+    print(test_pts_v4)
     print('Predicted v4 points:')
     print(pts3to4)
     print('Error:')
@@ -127,3 +140,28 @@ def test_fanc3_to_4(verbose=True):
     # return value based on that
 
 
+def test_fanc4_to_3():
+    pts4to3 = fanc4_to_3(test_pts_v4)
+    error = pts4to3 - test_pts_v3
+
+    print('True v3 points:')
+    print(test_pts_v3)
+    print('Predicted v3 points:')
+    print(pts4to3)
+    print('Error:')
+    print(error)
+
+    # TODO add some assertion about the error being small, and a True/False
+    # return value based on that
+
+
+def test(verbose=True):
+    pts_3_to_4_to_3 = fanc4_to_3(fanc3_to_4(test_pts_v3))
+    error343 = np.subtract(pts_3_to_4_to_3, test_pts_v3, dtype=float)
+    print('Error upon v3 -> v4 -> v3')
+    print(error343)
+
+    pts_4_to_3_to_4 = fanc3_to_4(fanc4_to_3(test_pts_v4))
+    error434 = np.subtract(pts_4_to_3_to_4, test_pts_v4, dtype=float)
+    print('Error upon v4 -> v3 -> v4')
+    print(error434)
