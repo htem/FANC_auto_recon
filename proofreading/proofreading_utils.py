b04b6577c4210dffd946856f3609a05dda4a682e
Brandon <bmark89@uw.edu>
2021-04-28 10:34:02 -0700

    added threshold for skeleton nodes present in a mesh in skel2seg

--- proofreading/proofreading_utils.py
+++ proofreading/proofreading_utils.py
@@ -10,6 +10,7 @@ import numpy as np
 
 def render_fragments(pts,
                      target_volume,
+                     threshold = 5
                      img_source = None,
                      seg_source = None,
                      state_server = None):
@@ -23,21 +24,24 @@ def render_fragments(pts,
     
     seg_ids = rootID_lookup.segIDs_from_pts(target_volume,pts)
     
- 
+    ids,counts = np.unique(seg_ids,return_counts=True)
+    value_counts = np.array(list(zip(ids,counts)),dtype=int)
+    ids_to_use = value_counts[value_counts[:,1]>threshold][:,0]
+    
     img_layer = ImageLayerConfig(img_source,name='Image_Layer')
 
 
     seg_layer = SegmentationLayerConfig(name = 'Segmentation_Layer',
                                    source = seg_source,
                                    selected_ids_column=None,
-                                   fixed_ids= np.unique(seg_ids),
+                                   fixed_ids= ids_to_use,
                                    active = False)
 
     state = StateBuilder([img_layer,seg_layer],resolution=[4.3,4.3,45],state_server=state_server)
     
     return state.render_state()
 
-def skel2seg(skeleton_id, project_id=13, copy_link=True, verbose=False):
+def skel2seg(skeleton_id, project_id=13, copy_link=True,threshold=5, verbose=False):
     """
     Given a skeleton ID of a skeleton in the FANC community CATMAID project
     (https://radagast.hms.harvard.edu/catmaidvnc/?pid=13), create a
@@ -59,13 +63,14 @@ def skel2seg(skeleton_id, project_id=13, copy_link=True, verbose=False):
         print('nodes_v4')
         print(nodes_v4)
 
+
     from cloudvolume import CloudVolume as CV
     production_seg = CV(
         authentication_utils.get_cv_path('FANC_production_segmentation')['url'],
         agglomerate=False
     )
 
-    neuroglancer_link = render_fragments(nodes_v4, production_seg)
+    neuroglancer_link = render_fragments(nodes_v4, production_seg, threshold)
     if copy_link:
         try:
             import pyperclip
