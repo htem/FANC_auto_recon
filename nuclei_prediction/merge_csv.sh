f6e1feb0eed7c462f3738fa3a200d6722542560a
sumiya-kuroda <26479377+sumiya-kuroda@users.noreply.github.com>
2021-07-15 02:54:21 +0000

    merge csv files from get_info_celbody.py

---
+++ nuclei_prediction/merge_csv.sh
@@ -0,0 +1,4 @@
+cd ../Output
+awk '(NR == 1) || (FNR > 1)' cellbody_cord_id_*.csv > merged.csv # leave first line (header) and merge everything else
+awk -F ',' 'NR==1 || !seen[$0]++' merged.csv > merged2.csv # remove completely same info
+awk -F ',' 'NR==1 || int($4) == 0 || !seen[$4]++' merged2.csv > info_cellbody.csv # remove rows with same segIDs (column4), but leave segID=0
