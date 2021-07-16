f3b021117462642d5734a3e7e5c0f021fd1424e6
sumiya-kuroda <26479377+sumiya-kuroda@users.noreply.github.com>
2021-07-16 02:11:50 +0000

    remove all zero rows

--- nuclei_prediction/merge_csv_4cellbody.sh
+++ nuclei_prediction/merge_csv_4cellbody.sh
@@ -1,4 +1,9 @@
-cd ../Output
+#!/bin/bash
+
+cd "$1"
+# cd /n/groups/htem/users/skuroda/nuclei_output
 awk '(NR == 1) || (FNR > 1)' cellbody_cord_id_*.csv > merged.csv # leave first line (header) and merge everything else
 awk -F ',' 'NR==1 || !seen[$0]++' merged.csv > merged2.csv # remove completely same info
-awk -F ',' 'NR==1 || int($4) == 0 || !seen[$4]++' merged2.csv > info_cellbody.csv # remove rows with same segIDs (column4), but leave segID=0
+awk -F ',' 'NR==1 || int($4) == 0 || !seen[$4]++' merged2.csv > merged3.csv # remove rows with same segIDs (column4), but leave segID=0
+awk -F ',' 'NR==1 || {s=0; for (i=1;i<=NF;i++) s+=$i; if (s!=0) print}' merged3.csv > info_cellbody.csv #remove all zero rows. 
+cp "$1"/info_cellbody.csv ~
--- nuclei_prediction/merge_csv_4neuron.sh
+++ nuclei_prediction/merge_csv_4neuron.sh
@@ -1,4 +1,8 @@
-cd ../Output
+#!/bin/bash
+
+cd "$1"
+# cd ../Output
 awk '(NR == 1) || (FNR > 1)' cellbody_and_neuron_*.csv > neuron_merged.csv # leave first line (header) and merge everything else
-awk -F ',' 'NR==1 || !seen[$0]++' neuron_merged.csv > info_neuron.csv # remove completely same info
-# awk -F ',' 'NR==1 || int($4) == 0 || !seen[$4]++' info_neuron.csv > info_neuron2.csv # remove rows with same segIDs (column4), but leave segID=0
+awk -F ',' 'NR==1 || !seen[$0]++' neuron_merged.csv > neuron_merged2.csv # remove completely same info
+awk -F ',' 'NR==1 || {s=0; for (i=1;i<=NF;i++) s+=$i; if (s!=0) print}' neuron_merged2.csv > info_neuron.csv #remove all zero rows. 
+cp "$1"/info_neuron.csv ~
