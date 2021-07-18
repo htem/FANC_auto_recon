c95d83c062bb21251caac1e4c058b184b374719b
sumiya-kuroda <26479377+sumiya-kuroda@users.noreply.github.com>
2021-07-18 07:45:24 +0000

    set wd to output folder

--- nuclei_prediction/listup_missed.sh
+++ nuclei_prediction/listup_missed.sh
@@ -2,13 +2,6 @@
 
 cd "$1"
 # cd ../Output
-awk '(NR == 1) || (FNR > 1)' cellbody_and_neuron_*.csv > neuron_merged.csv # leave first line (header) and merge everything else
-awk -F ',' 'NR==1 || !seen[$0]++' neuron_merged.csv > neuron_merged2.csv # remove completely same info
-awk -F ',' 'NR==1 || {s=0; for (i=1;i<=NF;i++) s+=$i; if (s!=0) print}' neuron_merged2.csv > info_neuron.csv #remove all zero rows. 
-cp "$1"/info_neuron.csv ~
-
-#! /bin/ksh
-
 exec > missing.txt
 for ((i=0 ; i<=55187 ; i++)) ; do
    if [ ! -f "cellbody_cord_id_$i.csv" ] ; then
@@ -16,3 +9,4 @@ for ((i=0 ; i<=55187 ; i++)) ; do
    fi
 done
 # grep '.....' missing.txt > after10000.txt
+cp "$1"/missing.txt ~
