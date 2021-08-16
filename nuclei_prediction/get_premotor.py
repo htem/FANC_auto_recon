40424bd8156e233a148bcca0aa604ac2cd6b7da8
sumiya-kuroda <26479377+sumiya-kuroda@users.noreply.github.com>
2021-08-16 08:54:33 +0000

    use db insted to get ID

--- nuclei_prediction/get_premotor.py
+++ nuclei_prediction/get_premotor.py
@@ -28,6 +28,7 @@ import config
 outputpath = '/n/groups/htem/users/skuroda'
 soma_table = '/home/skuroda/body_info_Aug2021.csv'
 syn_table = '/n/groups/htem/users/skuroda/full_VNC_synapses.csv'
+syn_table_db = '/n/groups/htem/users/skuroda/synapses.db'
 
 output_somadf_name = 'full_VNC_soma_20210816.csv'
 output_name = 'Premotor_20210816'
@@ -37,18 +38,20 @@ column_name = ['Seg ID','Synapses','Has soma?','Major merges fixed?','Major spli
 
 # update
 synaptic_links.update_synapse_tables(csv_path=syn_table)
+synaptic_links.update_synapse_tables(csv_path=syn_table, db_path=syn_table_db)
 config.update_soma_table(soma_table.rsplit('/', 1)[0], input_table_name='body_info_Aug2021', output_table_name=output_somadf_name)
 updated_soma_table = soma_table.rsplit('/', 1)[0] + '/' + output_somadf_name
 df = pd.read_csv(updated_soma_table, header=0)
+print('tables updated')
 
 # finds premotor inputs
-preMN = connectivity_utils.get_synapses(MN['pt_root_id'],syn_table,direction='inputs',threshold=1)
-
+preMN = connectivity_utils.get_synapses(MN['pt_root_id'],syn_table_db,direction='inputs',threshold=1)
+print('synapses into motor neurons found')
 temp = preMN['pre_root'].value_counts(ascending=False)
 preMN_table = pd.DataFrame(temp)
 preMN_table.columns = column_name[0:2]
 preMN_table = preMN_table.reindex(columns = preMN_table.columns.tolist() + column_name[2:])
-
+print('premotor inputs found')
 for i in range(len(preMN_table)):
     if preMN_table.loc[i,column_name[0]] in df['body_rootID']:
         preMN_table.loc[i,column_name[2]] = 'y'
