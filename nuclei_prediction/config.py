48fa6a03ce8ec273c99d3bfe586efbc20e5e301e
sumiya-kuroda <26479377+sumiya-kuroda@users.noreply.github.com>
2021-08-19 02:21:48 +0000

    now able to use timestamp

--- nuclei_prediction/config.py
+++ nuclei_prediction/config.py
@@ -125,17 +125,17 @@ def segID_to_svID(segID, ID_array, location_array_mip0, reverse=False):
     return svID[0],ptsj
 
     
-def update_soma_table(dir, input_table_name, output_table_name, cv=None, retry=True, max_tries=10, chunksize = 20000):
+def update_soma_table(dir, input_table_name, output_table_name, cv=None, timestamp=None, retry=True, max_tries=10, chunksize = 20000):
   if cv is None:
     cv = auth.get_cv()
 
   temp = dir + '/' + str(random.randint(111111,999999)) + '.csv'
   header = True
   idx = 0
-  for chunk in pd.read_csv(dir + '/{}.csv'.format(input_table_name), chunksize=chunksize): 
+  for chunk in pd.read_csv(dir + '/' + input_table_name, chunksize=chunksize): 
     try:
-      chunk.loc[:,'nuc_rootID'] = cv.get_roots(chunk.nuc_svID.values)
-      chunk.loc[:,'body_rootID'] = cv.get_roots(chunk.body_svID.values)
+      chunk.loc[:,'nuc_rootID'] = cv.get_roots(chunk.nuc_svID.values, timestamp=timestamp)
+      chunk.loc[:,'body_rootID'] = cv.get_roots(chunk.body_svID.values, timestamp=timestamp)
       chunk.to_csv(temp, mode='a', index=False, header=header)
       
     except Exception as e:
@@ -144,8 +144,8 @@ def update_soma_table(dir, input_table_name, output_table_name, cv=None, retry=T
         tries = 0
         while tries < max_tries:
           try:
-            chunk.loc[:,'nuc_rootID'] = cv.get_roots(chunk.nuc_svID.values)
-            chunk.loc[:,'body_rootID'] = cv.get_roots(chunk.body_svID.values)
+            chunk.loc[:,'nuc_rootID'] = cv.get_roots(chunk.nuc_svID.values, timestamp=timestamp)
+            chunk.loc[:,'body_rootID'] = cv.get_roots(chunk.body_svID.values, timestamp=timestamp)
             chunk.to_csv(temp, mode='a', index=False, header=False)
             tries = max_tries+1
           except Exception as e2:
--- nuclei_prediction/get_premotor.py
+++ nuclei_prediction/get_premotor.py
@@ -9,8 +9,7 @@ from tqdm import tqdm
 import argparse
 import sqlite3
 
-from PIL import Image
-import matplotlib.pyplot as plt
+from datetime import datetime
 import sys
 import os
 import csv
@@ -24,43 +23,81 @@ import synaptic_links
 sys.path.append(os.path.abspath("/home/skuroda/FANC_auto_recon/nuclei_prediction"))
 import config
 
-# setup paths and read csv files
+
+# paths and files
 outputpath = '/n/groups/htem/users/skuroda'
-soma_table = '/home/skuroda/body_info_Aug2021.csv'
-syn_table = '/n/groups/htem/users/skuroda/full_VNC_synapses.csv'
-syn_table_db = '/n/groups/htem/users/skuroda/synapses.db'
+updated_soma_fname = 'full_VNC_soma_20210615.csv'
+pMN_csv = None
+date = None # '2021-06-15'
+MN = '/home/skuroda/MN.csv'
+output_name = 'Premotor_20210615.csv'
 
-output_somadf_name = 'full_VNC_soma_20210816.csv'
-output_name = 'Premotor_20210816'
-MN = pd.read_csv('/home/skuroda/MN.csv', header=0)
+# original tables
+orig_soma = '/home/skuroda/body_info_Aug2021.csv'
+orig_syn_csv = '/n/groups/htem/users/skuroda/full_VNC_synapses.csv'
+orig_syn_db = '/n/groups/htem/users/skuroda/synapses.db'
 
+# other setups
 column_name = ['Seg ID','Synapses','Has soma?','Major merges fixed?','Major splits fixed?','Come back to me later','Other notes']
+if date != None:
+    dt_date = datetime.strptime(date, '%Y-%m-%d')
+    timestamp = int(dt_date.timestamp())
+else:
+    timestamp = None
 
-# update
+# update tables
 cv = auth.get_cv()
 
-synaptic_links.update_synapse_csv(syn_table,cv, max_tries=1000)
-synaptic_links.update_synapse_db(syn_table_db,syn_table)
-config.update_soma_table(soma_table.rsplit('/', 1)[0], input_table_name='body_info_Aug2021', output_table_name=output_somadf_name)
-updated_soma_table = soma_table.rsplit('/', 1)[0] + '/' + output_somadf_name
-df = pd.read_csv(updated_soma_table, header=0)
-print('tables updated')
+config.update_soma_table(orig_soma.rsplit('/', 1)[0], orig_soma.rsplit('/', 1)[1], updated_soma_fname, timestamp=timestamp)
+print('soma table updated')
 
-# finds premotor inputs
-preMN = connectivity_utils.get_synapses(MN['pt_root_id'],syn_table_db,direction='inputs',threshold=1)
-print('synapses into motor neurons found')
-temp = preMN['pre_root'].value_counts(ascending=False)
-preMN_table = pd.DataFrame(temp)
-preMN_table.columns = column_name[0:2]
-preMN_table = preMN_table.reindex(columns = preMN_table.columns.tolist() + column_name[2:])
-print('premotor inputs found')
-for i in range(len(preMN_table)):
-    if preMN_table.loc[i,column_name[0]] in df['body_rootID']:
-        preMN_table.loc[i,column_name[2]] = 'y'
+if pMN_csv == None:
+    synaptic_links.update_synapse_csv(orig_syn_csv,cv,max_tries=1000)
+    synaptic_links.update_synapse_db(orig_syn_db,orig_syn_csv)
+    print('synapse table updated')
+    MN_table = pd.read_csv(MN, header=0)
+    print('MN table read')
+
+    ## find premotor inputs
+    pMNs = connectivity_utils.get_synapses(MN_table['pt_root_id'],orig_syn_db,direction='inputs',threshold=1)
+    print('premotor synpases found')
+    temp = pMNs['pre_root'].value_counts(ascending=False)
+    pMN = pd.DataFrame(temp).reset_index()
+    pMN = pMN.reindex(columns = pMN.columns.tolist() + column_name[2:])
+    pMN.columns = column_name
+    print('premotor neurons found')
+
+else:
+    pMN = pd.read_csv(pMN_csv, header=0)
+    print('premotor neurons read')
+
+# find nuclei of premotor inputs
+updated_soma = orig_soma.rsplit('/', 1)[0] + '/' + updated_soma_fname
+df = pd.read_csv(updated_soma, header=0)
+
+for i in range(len(pMN)):
+    if pMN.loc[i,column_name[0]] in df['body_rootID'].values:
+        pMN.loc[i,column_name[2]] = 'y'
     else:
         pass
 
-num_with_nuclei = (preMN_table[column_name[2]] == 'y').sum()
-output = preMN_table.fillna("")
-output.to_csv(outputpath + '/' + '{}.csv'.format(output_name), index=False)
-print('{num} premotor inputs ({percentage}%) have nuclei'.format(num=num_with_nuclei, percentage=num_with_nuclei/len(preMN_table)))
+obj_with_nuclei = (pMN[column_name[2]] == 'y').sum()
+syn_with_nuclei = pMN[(pMN[column_name[2]] == 'y')][column_name[1]].sum()
+output = pMN.fillna("")
+output.to_csv(outputpath + '/' + output_name, index=False)
+
+text_o = """\
+    as of {date}
+    {A} objects out of {B} premotor inputs have nuclei (~{C}%)
+    {D} synapses out of {E} premotor inputs have nuclei (~{F}%)\
+    """.format(date=date or 'now',
+                A=obj_with_nuclei,
+                B=len(pMN),
+                C=obj_with_nuclei*100/len(pMN),
+                D=syn_with_nuclei,
+                E=pMN[column_name[1]].sum(),
+                F=syn_with_nuclei*100/pMN[column_name[1]].sum())
+
+print(text_o)
+
+
