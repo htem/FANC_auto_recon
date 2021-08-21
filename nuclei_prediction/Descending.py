b55eaf91d6bf86df60f3503765c0d201e2a601d8
sumiya-kuroda <26479377+sumiya-kuroda@users.noreply.github.com>
2021-08-21 07:28:41 +0000

    savig DA output

--- nuclei_prediction/Descending.py
+++ nuclei_prediction/Descending.py
@@ -23,11 +23,13 @@ import synaptic_links
 sys.path.append(os.path.abspath("/home/skuroda/FANC_auto_recon/nuclei_prediction"))
 import config
 
+# what data u want
+getobj = False
+getsyn = True
 
 # paths and files
 outputpath = '/n/groups/htem/users/skuroda/da'
 updated_soma_fname = 'full_VNC_soma_da0820.csv'
-pMN_csv = None
 date = None # '2021-06-15'
 MN = '/home/skuroda/MN.csv'
 
@@ -44,53 +46,54 @@ if date != None:
 else:
     timestamp = None
 
-# update tables
-print('updating soma table...')
-config.update_soma_table(orig_soma.rsplit('/', 1)[0], orig_soma.rsplit('/', 1)[1], updated_soma_fname, timestamp=timestamp)
-print('soma table updated')
-updated_soma = orig_soma.rsplit('/', 1)[0] + '/' + updated_soma_fname
-df = pd.read_csv(updated_soma, header=0)
+if getobj == True:
+    # update tables
+    print('updating soma table...')
+    # config.update_soma_table(orig_soma.rsplit('/', 1)[0], orig_soma.rsplit('/', 1)[1], updated_soma_fname, timestamp=timestamp)
+    print('soma table updated')
+    updated_soma = orig_soma.rsplit('/', 1)[0] + '/' + updated_soma_fname
+    df = pd.read_csv(updated_soma, header=0)
 
-# get segID of descending and ascending
-print('retrieving segIDs of descending and ascending neurons...')
-LHSda_df = pd.read_csv(outputpath + '/LHS75200Seeds.csv', header=0)
-RHSda_df = pd.read_csv(outputpath + '/RHS75200Seeds.csv', header=0)
+    # get segID of descending and ascending
+    print('retrieving segIDs of descending and ascending neurons...')
+    LHSda_df = pd.read_csv(outputpath + '/LHS75200Seeds.csv', header=0)
+    RHSda_df = pd.read_csv(outputpath + '/RHS75200Seeds.csv', header=0)
 
-LHSxyz = LHSda_df['Coordinate 1'].str.strip('()').str.split(',',expand=True)
-RHSxyz = RHSda_df['Coordinate 1'].str.strip('()').str.split(',',expand=True)
+    LHSxyz = LHSda_df['Coordinate 1'].str.strip('()').str.split(',',expand=True)
+    RHSxyz = RHSda_df['Coordinate 1'].str.strip('()').str.split(',',expand=True)
 
-LHSda = IDlook.segIDs_from_pts_cv(LHSxyz.astype(int).to_numpy(),cv)
-RHSda = IDlook.segIDs_from_pts_cv(RHSxyz.astype(int).to_numpy(),cv)
+    LHSda = IDlook.segIDs_from_pts_cv(LHSxyz.astype(int).to_numpy(),cv)
+    RHSda = IDlook.segIDs_from_pts_cv(RHSxyz.astype(int).to_numpy(),cv)
 
-LHSda_indices = np.isin(LHSda.astype('int64'), df['body_rootID'].values)
-left_d = LHSda[LHSda_indices]
-left_a = LHSda[np.logical_not(LHSda_indices)]
+    LHSda_indices = np.isin(LHSda.astype('int64'), df['body_rootID'].values)
+    left_d = LHSda[LHSda_indices]
+    left_a = LHSda[np.logical_not(LHSda_indices)]
 
-RHSda_indices = np.isin(RHSda.astype('int64'), df['body_rootID'].values)
-right_d = RHSda[RHSda_indices]
-right_a = RHSda[np.logical_not(RHSda_indices)]
+    RHSda_indices = np.isin(RHSda.astype('int64'), df['body_rootID'].values)
+    right_d = RHSda[RHSda_indices]
+    right_a = RHSda[np.logical_not(RHSda_indices)]
 
-out1 = left_d.astype(np.int64)
-out1.tofile(outputpath + '/' + '{}.bin'.format('left_d'))
-out2 = left_a.astype(np.int64)
-out2.tofile(outputpath + '/' + '{}.bin'.format('left_a'))
-out3 = right_d.astype(np.int64)
-out3.tofile(outputpath + '/' + '{}.bin'.format('right_d'))
-out4 = right_a.astype(np.int64)
-out4.tofile(outputpath + '/' + '{}.bin'.format('right_a'))
+    out1 = left_d.astype(np.int64)
+    out1.tofile(outputpath + '/' + '{}.bin'.format('left_d'))
+    out2 = left_a.astype(np.int64)
+    out2.tofile(outputpath + '/' + '{}.bin'.format('left_a'))
+    out3 = right_d.astype(np.int64)
+    out3.tofile(outputpath + '/' + '{}.bin'.format('right_d'))
+    out4 = right_a.astype(np.int64)
+    out4.tofile(outputpath + '/' + '{}.bin'.format('right_a'))
 
-
-if pMN_csv == None:
+    # update synapse table
     print('updating synapse table...')
-    synaptic_links.update_synapse_csv(orig_syn_csv,cv,max_tries=100000)
-    synaptic_links.update_synapse_db(orig_syn_db,orig_syn_csv)
+    # synaptic_links.update_synapse_csv(orig_syn_csv,cv,max_tries=100000)
+    # synaptic_links.update_synapse_db(orig_syn_db,orig_syn_csv)
     print('synapse table updated')
+
+    # find premotor inputs
     MN_table = pd.read_csv(MN, header=0)
     MNT1L = MN_table[MN_table['name'].str.endswith('T1L')]
     MNT1R = MN_table[MN_table['name'].str.endswith('T1R')]
     print('MN table read')
 
-    ## find premotor inputs
     pMN_T1Ls = connectivity_utils.get_synapses(MNT1L['pt_root_id'],orig_syn_db,direction='inputs',threshold=2)
     pMN_T1Rs = connectivity_utils.get_synapses(MNT1R['pt_root_id'],orig_syn_db,direction='inputs',threshold=2)
     print('premotor synpases found')
@@ -102,8 +105,58 @@ if pMN_csv == None:
     pMN_T1R.columns = ['root_id','synapses']
     print('premotor neurons found')
 
-else:
-    pass
+    pMN_T1L.to_csv(outputpath + '/pMN_T1L.csv', index=False)
+    pMN_T1R.to_csv(outputpath + '/pMN_T1R.csv', index=False)
 
-pMN_T1L.to_csv(outputpath + '/pMN_T1L.csv', index=False)
-pMN_T1R.to_csv(outputpath + '/pMN_T1R.csv', index=False)
+
+if getsyn == True:
+    # import data
+    left_d = np.fromfile(outputpath + '/left_d.bin', dtype=np.int64)
+    left_a = np.fromfile(outputpath + '/left_a.bin', dtype=np.int64)
+    right_d = np.fromfile(outputpath + '/right_d.bin', dtype=np.int64)
+    right_a = np.fromfile(outputpath + '/right_a.bin', dtype=np.int64)
+    DAN = np.concatenate((left_d, left_a, right_d, right_a)) # np.unique just in case?
+    print('descending & ascending neuron table read')
+
+    MN_table = pd.read_csv(MN, header=0)
+    print('MN table read')
+
+
+    # calculate bumber of synapses for each connection
+    print('inputs to MNs looking...')
+    MN_df = connectivity_utils.get_synapses(MN_table['pt_root_id'],orig_syn_db,direction='inputs',threshold=2)
+    MN_inputs = []
+    for i in MN_table['pt_root_id']:
+        tempi = MN_df[MN_df['post_root'] == i]
+        result = np.zeros(len(DAN)+1, dtype='int64')
+        for j in range(len(DAN)):
+            result[j] = np.count_nonzero(tempi['pre_root'].values() == DAN[j])
+        result[len(DAN)] = len(tempi) - result[0:len(DAN)-1].sum()
+        MN_inputs.append(result)
+
+    MN_inputs_df = np.array(MN_inputs)
+    o = pd.DataFrame(data=MN_inputs_df,
+                    index=MN_table['pt_root_id'],
+                    columns=DAN + 'others')
+
+    o.to_csv(outputpath + '/MNinputs.csv', index=True)
+    print('inputs to MNs saved')
+    
+    print('outputs of DA neurons looking...')
+    DAN_df = connectivity_utils.get_synapses(DAN,orig_syn_db,direction='outputs',threshold=2)
+    DAN_outputs = []
+    for i in DAN:
+        tempi = DAN_df[DAN_df['pre_root'] == i]
+        result = np.zeros(len(MN_table)+1, dtype='int64')
+        for j in range(len(MN_table)):
+            result[j] = np.count_nonzero(tempi['post_root'].values() == MN_table['pt_root_id'][j])
+        result[len(MN_table)] = len(tempi) - result[0:len(MN_table)-1].sum()
+        DAN_outputs.append(result)
+
+    DAN_outputs_df = np.array(DAN_outputs)
+    o = pd.DataFrame(data=DAN_outputs_df,
+                    index=DAN,
+                    columns=MN_table['pt_root_id'] + 'others')
+
+    o.to_csv(outputpath + '/DANoutputs.csv', index=True)
+    print('outputs of DA neurons saved')
