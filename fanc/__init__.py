740c7df30ea545665198538be876d1a290b58f22
Jasper Phelps <jasper.s.phelps@gmail.com>
2022-12-03 23:48:37 +0100

    Rename proofreading_utils to statebuilder

--- fanc/__init__.py
+++ fanc/__init__.py
@@ -1,6 +1,19 @@
-__all__ = ['auth', 'catmaid_utilities', 'connectivity_utils', 'ngl_info',
-           'proofreading_utils', 'rootID_lookup', 'schema_download',
-           'schema_upload', 'skeletonization', 'statemanager',
-           'synaptic_links', 'transforms', 'visualize']
+#!/usr/bin/env python3
+
+__all__ = [
+    'auth',
+    'catmaid_utilities',
+    'connectivity_utils',
+    'ngl_info',
+    'rootID_lookup',
+    'schema_download',
+    'schema_upload',
+    'skeletonization',
+    'statebuilder',
+    'statemanager',
+    'synaptic_links',
+    'transforms',
+    'visualize'
+]
 
 from . import *
