6bcafbc46eed066a18d0c715a5ab3fa7ffe57ced
Jasper Phelps <jasper.s.phelps@gmail.com>
2022-12-05 22:50:21 +0100

    Restructure `__init__.py`, and directly import some functions

--- fanc/__init__.py
+++ fanc/__init__.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 
-__all__ = [
-    'auth',
-    'catmaid',
-    'connectivity',
-    'ngl_info',
-    'render_neurons',
-    'rootID_lookup',
-    'schema_download',
-    'schema_upload',
-    'skeletonize',
-    'statebuilder',
-    'statemanager',
-    'synaptic_links',
-    'template_spaces',
-    'transforms',
-    'visualize'
-]
+from . import (
+    auth,
+    catmaid,
+    connectivity,
+    ngl_info,
+    rootID_lookup,
+    schema_download,
+    schema_upload,
+    skeletonize,
+    statebuilder,
+    statemanager,
+    synaptic_links,
+    template_spaces,
+    transforms,
+)
 
-from . import *
+from .render_neurons import render_neuron_into_template_space
+from .visualize import plot_neurons
