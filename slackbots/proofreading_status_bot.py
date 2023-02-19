35635c88885abab2e464a1fda8a7249393d54aad
Jasper Phelps <jasper.s.phelps@gmail.com>
2023-02-20 00:51:51 +0100

    First functional version of `proofreading_status_bot.py`

--- slackbots/proofreading_status_bot.py
+++ slackbots/proofreading_status_bot.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 """
-Slack API info:
-    https://api.slack.com/messaging/retrieving
-    https://api.slack.com/messaging/sending
 Install the slack python package with `pip install slack_sdk`
+Some useful Slack API info pages:
+  - https://api.slack.com/messaging/retrieving
+  - https://api.slack.com/messaging/sending
 
-View your slack apps:
-    https://api.slack.com/apps
-Save your app's auth token to your shell environment by adding a line like this
-to your shell startup file (e.g. ~/.bashrc, ~/.zshrc):
+View your slack apps: https://api.slack.com/apps
+This app requires the following permissions:
+  - channels:history
+  - channels:read
+  - chat:write
+Save your app's auth token to your shell environment by adding a line
+like this to your shell startup file (e.g. ~/.bashrc, ~/.zshrc):
     export SLACK_BOT_TOKEN=xoxb-123456789012-...
 """
 
@@ -22,9 +25,12 @@ import numpy as np
 import pandas as pd
 from caveclient import CAVEclient
 
+import fanc
+
 
 caveclient = CAVEclient('fanc_production_mar2021')
 info = caveclient.info.get_datastack_info()
+proofreading_table = 'proofreading_status_jasper'
 
 if len(sys.argv) > 1 and sys.argv[1] in os.environ:
     token = os.environ[sys.argv[1]]
@@ -33,7 +39,7 @@ else:
 slackclient = slack_sdk.WebClient(token=token)
 
 # Build some dicts that make it easy to look up IDs for users and channels
-#all_users = client.users_list()['members']
+#all_users = slackclient.users_list()['members']
 #userid_to_username = {
 #    user['id']: user['profile']['display_name'].lower()
 #    if user['profile']['display_name'] else user['profile']['real_name'].lower()
@@ -45,6 +51,7 @@ channelname_to_channelid = {x['name']: x['id'] for x in all_conversations}
 channelid_to_channelname = {x['id']: x['name'] for x in all_conversations}
 
 channel_id = channelname_to_channelid['proofreading-status-bot']  # returns 'C04Q90KGGRH'
+
 
 def show_help():
     return (
@@ -63,6 +70,7 @@ A segment ID followed by a `!` followed by an xyz point coordinate (typically co
 
 • These examples use the segment ID `648518346481082458` but you should substitute this with the segment ID that you're interested in.
 • If you want to confirm the bot is working properly, try sending the first example message to the channel and make sure you get a response.
+• If you want to add a large number of entries to the proofreading status table, you can contact Jasper directly instead of using this bot.
 """)
 
 
@@ -86,15 +94,96 @@ def process_message(message: str) -> str:
     """
     tokens = message.strip(' ').split(' ')
     if len(tokens) == 0:
-        return ("Doing nothing: Your message is empty or I couldn't understand"
+        return ("NO ACTION: Your message is empty or I couldn't understand"
                 " it. Make a post containing the word 'help' if needed.")
     segid = tokens[0][:-1]
     if tokens[0].endswith('?'):
+        # Query
         return f"Querying {segid} (to be implemented)"
     elif tokens[0].endswith('!'):
-        return f"Posting {segid} (to be implemented)"
+        # Upload
+        try:
+            segid = int(tokens[0][:-1])
+        except ValueError:
+            return (f"ERROR: Could not convert the first word"
+                    f" {tokens[0][:-1]} to int. Is it a segID?")
+        if not caveclient.chunkedgraph.is_latest_roots(segid):
+            return (f"ERROR: {segid} is not a current segment ID."
+                    " Was the segment edited recently? Or did you"
+                    " copy-paste the wrong thing?")
+        if have_recently_uploaded(segid):
+            return (f"ERROR: I recently uploaded segment ID {segid}."
+                    " I'm not going to upload it again.")
+        # TODO TODO TODO add a check to see if the segid is already in the
+        # proofreading table, using live_live_query
+
+        soma = fanc.lookup.somas_from_segids(segid, timestamp='now')
+        if len(soma) > 1:
+            return (f"ERROR: Segment ID {segid} has multiple entires"
+                    " in the soma table, with the coordinates listed"
+                    " below. Shame on you for marking a cell as"
+                    " proofread when it still has two somas! (Or"
+                    " there's a bug in my code.)\n\n"
+                    f"{np.vstack(soma.pt_position)}")
+        elif len(soma) == 0 and len(tokens) == 1:
+            return ("ERROR: Segment ID {segid} has no entry in the soma"
+                    " table.\n\nIf you clearly see a soma attached to"
+                    " this object, probably the automated soma detection"
+                    " algorithm missed this soma. If so, tag Sumiya"
+                    " Kuroda here and he can add it to the soma table."
+                    "\n\nIf you're sure this is a descending neuron or"
+                    " a sensory neuron, you can specify a point to"
+                    "anchor the proofreading annotation. Call 'help'"
+                    " for details.")
+        elif len(soma) == 0 and len(tokens) != 4:
+            return ("ERROR: You did not provide a segment ID followed"
+                    " by an xyz point coordinate, at least not in the"
+                    " expected format.")
+        elif len(soma) == 0 and len(tokens) == 4:
+            try:
+                point = [float(i.strip(',')) for i in tokens[1:]]
+            except ValueError:
+                return (f"ERROR: Could not convert the last 3 words to"
+                        " integers. Are they point coordinates?"
+                        f"\n\n`{[i for i in tokens[1:]]}`")
+            segid_from_point = fanc.lookup.segids_from_pts(pt)
+            if not segid_from_point == segid:
+                return (f"ERROR: The provided point {point} is inside"
+                        f" segment ID {segid_from_point} which doesn't"
+                        f" match the segid you provided {segid}.")
+
+        elif len(soma) == 1 and len(tokens) > 1:
+            return (f"ERROR: Segment ID {segid} has an entry in the"
+                    f" soma table at {list(np.hstack(soma.pt_position))}"
+                    " but you provided additional information."
+                    " Additional information is unexpected when the"
+                    " segment has a soma, so I didn't do anything.")
+        else:
+            point = list(np.hstack(soma.pt_position))
+
+        stage = caveclient.annotation.stage_annotations(proofreading_table)
+        try:
+            stage.add(
+                proofread=True,
+                pt_position=point,
+                user_id=2660,  # Hardcoded to be Jasper until I figure out a way to
+                               # determine the CAVE user ID automatically from the
+                               # Slack user name
+                valid_id=segid
+            )
+        except Exception as e:
+            return (f"ERROR: Staging failed with exception {type(e)} {e}")
+
+        try:
+            response = caveclient.annotation.upload_staged_annotations(stage)
+            record_upload(segid)
+            return (f"Upload succeeded for segment ID {segid} and point"
+                    f" coordinate {point}.\n\nServer response: {response}")
+        except Exception as e:
+            return (f"ERROR: Upload failed with exception {type(e)} {e}")
+
     else:
-        return ("Doing nothing: The first word in your message isn't a segment"
+        return ("NO ACTION: The first word in your message isn't a segment"
                 " ID terminated by a ! or a ?. Make a post containing the word"
                 " 'help' if needed.")
     
@@ -133,6 +222,18 @@ def fetch_messages_and_post_replies(verbosity=1, fake=False):
             thread_ts=message['ts'],
             text=response
         )
+
+
+def record_upload(segid, uploads_fn='proofreading_status_bot_uploads.txt'):
+    with open(uploads_fn, 'a') as f:
+        f.write(f'{segid}\n')
+
+
+def have_recently_uploaded(segid, uploads_fn='proofreading_status_bot_uploads.txt'):
+    with open(uploads_fn, 'r') as f:
+        recent_uploads = [int(line.strip()) for line in f.readlines()]
+    return segid in recent_uploads
+
 
 if __name__ == '__main__':
     while True:
