895eb58012a1ef3e2c67efbe961d573336186198
Jasper Phelps <jasper.s.phelps@gmail.com>
2023-09-12 17:06:06 +0200

    Add a deleted cell ID
    I should really make `fanc.upload.new_cell()` just be able to figure out
    what IDs are invalid, but until I do that, I just have to occasionally
    update this list of deleted cell IDs.

--- fanc/upload.py
+++ fanc/upload.py
@@ -53,7 +53,7 @@ def new_cell(pt_position,
     start_id = start_ids[cell_type]
     # Annotations that were deleted aren't materialized so they won't be in the
     # cell_ids dataframe, but new annotations can't re-use their IDs.
-    deleted_cell_ids = [10552, 10766, 13325]
+    deleted_cell_ids = [1815, 10552, 10766, 13325]
     while start_id in cell_ids['id'].values or start_id in deleted_cell_ids:
         start_id += 1
     stage = client.annotation.stage_annotations(cell_ids_table, id_field=True)
