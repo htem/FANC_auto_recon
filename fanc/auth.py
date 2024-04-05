f9e37885a69742b54f525e469f57f1250cf00ef3
Jasper Phelps <jasper.s.phelps@gmail.com>
2024-04-05 14:51:27 +0200

    Refactor `auth.py`, mainly to add support for changing `auth_token_key`
    Technically this is a breaking change because I changed a keyword
    argument name, but it's very minor and a rarely used argument so I think
    users will be able to figure out what they need to do if they do run
    into this kwarg change

--- fanc/auth.py
+++ fanc/auth.py
@@ -1,70 +1,100 @@
 #!/usr/bin/env python3
 
 import os
-from pathlib import Path
-import json
 
 from caveclient import CAVEclient
 from cloudvolume import CloudVolume
 from meshparty import trimesh_io
 
+DEFAULT_DATASET = 'fanc_production_mar2021'
 DATASTACK_NICKNAMES = {
+    'fanc': 'fanc_production_mar2021',
     'production': 'fanc_production_mar2021',
     'sandbox': 'fanc_sandbox'
 }
 
-DEFAULT_MESH_CACHE = os.path.expanduser('~/fanc-meshes')
+configs = {
+    'mesh_cache': os.path.expanduser('~/fanc-meshes'),
+    'cave_auth_token_key': 'fanc_production_mar2021',
+}
+if os.environ.get('FANC_AUTH_TOKEN_KEY'):
+    configs['cave_auth_token_key'] = os.environ['FANC_AUTH_TOKEN_KEY']
 
-# To enable lazy loading of CAVEclients and cloudvolumes
+# To enable lazy loading and caching of CAVEclients and cloudvolumes
 _clients = {}
 _cloudvolumes = {}
 
 
-def save_cave_credentials(token, dataset='fanc_production_mar2021', overwrite=False):
+def save_cave_credentials(token,
+                          token_key=DEFAULT_DATASET,
+                          overwrite=False):
     # If a nickname was used, get the proper datastack name
-    dataset = DATASTACK_NICKNAMES.get(dataset, dataset)
+    token_key = DATASTACK_NICKNAMES.get(token_key, token_key)
 
     client = CAVEclient()
-    client.auth.save_token(token, token_key=dataset, overwrite=overwrite)
+    client.auth.save_token(token, token_key=token_key, overwrite=overwrite)
     try:
         client.auth.save_token(token, token_key='token', overwrite=False)
     except KeyError:
         print('Global credentials (with key "token") already set, will not overwrite')
 
     print('Token succesfully stored at: '
-          f'~/.cloudvolume/secrets/cave-secret.json under key "{dataset}"')
+          f'~/.cloudvolume/secrets/cave-secret.json under key "{token_key}"')
 
 
-def get_caveclient(dataset='fanc_production_mar2021'):
+def use_auth_token_key(token_key):
+    """
+    Set the auth token key to use for all subsequent calls to get_caveclient()
+
+    Note that existing clients that you got from calling get_caveclient() will
+    not be updated. Call get_caveclient() again after calling this function to
+    get a client with the new auth token key.
+    """
+    configs['cave_auth_token_key'] = token_key
+
+
+def get_caveclient(dataset=DEFAULT_DATASET, auth_token_key=None):
     # If a nickname was used, get the proper datastack name
     dataset = DATASTACK_NICKNAMES.get(dataset, dataset)
 
+    # If auth_token_key is not provided, use the default
+    if auth_token_key is None:
+        auth_token_key = configs['cave_auth_token_key']
+
     if dataset not in _clients:
-        _clients[dataset] = CAVEclient(dataset, auth_token_key=dataset)
+        _clients[dataset] = {}
+    if auth_token_key not in _clients[dataset]:
+        _clients[dataset][auth_token_key] = CAVEclient(dataset,
+                                                       auth_token_key=auth_token_key)
 
-    return _clients[dataset]
+    return _clients[dataset][auth_token_key]
 
 
-def get_cloudvolume(dataset='fanc_production_mar2021'):
+def get_chunkedgraph_path(dataset=DEFAULT_DATASET):
+    client = get_caveclient(dataset=dataset)
+    return client.info.segmentation_source().replace('middleauth+', '')
+
+
+def get_cloudvolume(dataset=DEFAULT_DATASET):
     # If a nickname was used, get the proper datastack name
     dataset = DATASTACK_NICKNAMES.get(dataset, dataset)
 
     if dataset not in _cloudvolumes:
-        client = get_caveclient(dataset=dataset)
-
         _cloudvolumes[dataset] = CloudVolume(
-            client.info.get_datastack_info()['segmentation_source'].replace('middleauth+', ''),
+            get_chunkedgraph_path(dataset),
             use_https=True,
-            secrets=client.auth.token
+            secrets=get_caveclient(dataset).auth.token
         )
-
     return _cloudvolumes[dataset]
 
 
-def get_meshmanager(dataset='fanc_production_mar2021',
-                    mesh_cache=DEFAULT_MESH_CACHE):
+def get_meshmanager(dataset=DEFAULT_DATASET,
+                    mesh_cache=None):
+    if mesh_cache is None:
+        mesh_cache = configs['mesh_cache']
+
     return trimesh_io.MeshMeta(
-        cv_path=get_caveclient().info.segmentation_source(),
+        cv_path=get_chunkedgraph_path(dataset),
         disk_cache_path=mesh_cache,
         map_gs_to_https=True
     )
